3803257b490a3c7cf296496fdf68ba952034164b
caesario <32963391+caesario@users.noreply.github.com>
2018-06-27 03:06:41 +0700

    Create Blog

---
+++ application/user/views/Blog.php
@@ -0,0 +1,86 @@
+<?php
+include "layout/Header.php";
+include "layout/Menu.php";
+?>
+
+    <div class="wrapper-cart">
+        <h5 class="text-center c-title-cart">BLOG</h5>
+        <div class="c-breadcrumb text-center c-bread-padding">
+            <p></p>
+        </div>
+    </div>
+
+    <div class="container-fluid c-padding-header">
+        <div class="row">
+            <!-- ======= Side Kiri ======= -->
+            <div class="col-lg-3">
+                <h5>Post Terbaru</h5>
+                <hr>
+                <div class="media mb-3">
+                    <div class="media-left">
+                        <a href="" class="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">
+                            <img width="74" height="74" src="http://themepiko.com/demo/stock/default/wp-content/uploads/2016/12/blog01-150x150.jpg" class="attachment-64x64 size-64x64 wp-post-image" alt="">                                </a>
+                    </div>
+                    <div class="media-body">
+                        <p class="c-cart-productname ml-3"><a href="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">Have you seen these stunning breakthroughs in 3D</a></p>
+                        <p class="c-text-footer m-0 ml-3">December 1, 2018</p>
+                    </div>
+                </div>
+                <div class="media mb-3">
+                    <div class="media-left">
+                        <a href="" class="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">
+                            <img width="74" height="74" src="http://themepiko.com/demo/stock/default/wp-content/uploads/2016/12/blog01-150x150.jpg" class="attachment-64x64 size-64x64 wp-post-image" alt="">                                </a>
+                    </div>
+                    <div class="media-body">
+                        <p class="c-cart-productname ml-3"><a href="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">Have you seen these stunning breakthroughs in 3D</a></p>
+                        <p class="c-text-footer m-0 ml-3">December 1, 2018</p>
+                    </div>
+                </div>
+                <div class="media mb-3">
+                    <div class="media-left">
+                        <a href="" class="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">
+                            <img width="74" height="74" src="http://themepiko.com/demo/stock/default/wp-content/uploads/2016/12/blog01-150x150.jpg" class="attachment-64x64 size-64x64 wp-post-image" alt="">                                </a>
+                    </div>
+                    <div class="media-body">
+                        <p class="c-cart-productname ml-3"><a href="" rel="bookmark" title="Have you seen these stunning breakthroughs in 3D">Have you seen these stunning breakthroughs in 3D</a></p>
+                        <p class="c-text-footer m-0 ml-3">December 1, 2018</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ======= Side Kiri ======= -->
+            <div class="col-lg-6 col-sm-12 ml-lg-5">
+                <div class="card mb-4">
+                    <div class="card-body pt-1">
+                        <h4 class="card-title mb-2">Special title treatment</h4>
+                        <p class="c-text-footer m-0">December 1, 2018</p>
+                        <p class="card-text c-text-blog">With supporting text below as a natural lead-in lorem to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa fugiat, laborum maxime molestias omnis quos? Accusamus aliquam commodi debitis, distinctio doloribus eius illo libero provident qui quod soluta veniam vero.</p>
+                        <a href="https://dev.eazy-dev.xyz/kuze/Alamat" class="btn btn-csr c-btn-blog mt-1 float-right">Read More</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="card mb-4">
+                    <div class="card-body pt-1">
+                        <h4 class="card-title mb-2">Special title treatment</h4>
+                        <p class="c-text-footer m-0">December 1, 2018</p>
+                        <p class="card-text c-text-blog">With supporting text below as a natural lead-in lorem to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa fugiat, laborum maxime molestias omnis quos? Accusamus aliquam commodi debitis, distinctio doloribus eius illo libero provident qui quod soluta veniam vero.</p>
+                        <a href="https://dev.eazy-dev.xyz/kuze/Alamat" class="btn btn-csr c-btn-blog mt-1 float-right">Read More</a>
+                    </div>
+                </div>
+                <hr>
+                <div class="card mb-4">
+                    <div class="card-body pt-1">
+                        <h4 class="card-title mb-2">Special title treatment</h4>
+                        <p class="c-text-footer m-0">December 1, 2018</p>
+                        <p class="card-text c-text-blog">With supporting text below as a natural lead-in lorem to additional content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa fugiat, laborum maxime molestias omnis quos? Accusamus aliquam commodi debitis, distinctio doloribus eius illo libero provident qui quod soluta veniam vero.</p>
+                        <a href="https://dev.eazy-dev.xyz/kuze/Alamat" class="btn btn-csr c-btn-blog mt-1 float-right">Read More</a>
+                    </div>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+
+<?php
+include "layout/Footer.php";
+?>
