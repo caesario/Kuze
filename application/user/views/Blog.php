c62390090869e2e8351cfd90ebbc52756d017c88
caesario <32963391+caesario@users.noreply.github.com>
2018-06-27 19:27:12 +0700

    Update Blog & Artikel

--- application/user/views/Blog.php
+++ application/user/views/Blog.php
@@ -80,6 +80,21 @@ include "layout/Menu.php";
                     </div>
                 </div>
                 <hr>
+                <div class="">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-end">
+                            <li class="page-item c-pagination disabled">
+                                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                            </li>
+                            <li class="page-item c-pagination"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item c-pagination"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item c-pagination"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item c-pagination">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
