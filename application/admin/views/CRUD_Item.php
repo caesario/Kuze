e273ea20b17494bf5ac2d7574fa38380c1879540
Muhammad Irfan <funnyirfan321@gmail.com>
2018-07-07 01:34:44 +0700

    Update

--- application/admin/views/CRUD_Item.php
+++ application/admin/views/CRUD_Item.php
@@ -232,7 +232,7 @@ if ($submit == 'Ubah') {
             plugins: [
                 'advlist autolink lists link image charmap print preview anchor textcolor',
                 'searchreplace visualblocks code fullscreen',
-                'insertdatetime media table contextmenu paste code help wordcount'
+                'insertdatetime media table paste code help wordcount'
             ],
             toolbar: 'undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
             content_css: [
