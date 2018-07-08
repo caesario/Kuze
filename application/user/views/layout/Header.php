fe23fae3c1121de8b7384291050b75a1ee8f711a
Muhammad Irfan <funnyirfan321@gmail.com>
2018-07-08 23:35:45 +0700

    Update

--- application/user/views/layout/Header.php
+++ application/user/views/layout/Header.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/eazy-style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/font-awesome.css'); ?>">
     <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="<?= site_url('assets/vendor/select2/select2-bootstrap4.min.css'); ?>" />
+    <link rel="stylesheet" href="<?= site_url('assets/vendor/datatable/css/dataTables.bootstrap4.min.css'); ?>"/>
     <link rel="stylesheet" href="<?= base_url('assets/vendor/fotorama/fotorama.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/eazy-style.css') ?>">
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
