2825bc1d753d1440a9ecee5d612a47dbec639812
Muhammad Irfan <irfan@bukaide.com>
2018-09-03 23:07:21 +0700

    Update

--- application/admin/controllers/Item.php
+++ application/admin/controllers/Item.php
@@ -106,7 +106,7 @@ class Item extends MY_Controller
             'i_hrg' => $this->input->post('hrg'),
             'i_berat' => $this->input->post('berat'),
             'i_deskripsi' => $this->input->post('deskripsi'),
-            'i_hot' => 0,
+            'i_sale' => $this->input->post('i_sale') != '' ? $this->input->post('i_sale') : 0,
             'i_new' => $this->input->post('i_new') != '' ? $this->input->post('i_new') : 0,
             'i_best' => $this->input->post('i_best') != '' ? $this->input->post('i_best') : 0,
             'i_url' => $this->slug->create_uri(array('title' => $this->input->post('nama')))
