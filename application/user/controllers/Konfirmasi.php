42cc499aa05c11c9f8cccde78bc06707d3c2f0e1
Muhammad Irfan <irfan@bukaide.com>
2018-09-25 23:48:33 +0700

    Update

--- application/user/controllers/Konfirmasi.php
+++ application/user/controllers/Konfirmasi.php
@@ -157,62 +157,100 @@ class Konfirmasi extends MY_Controller
 
     public function simpan()
     {
-        $orders_noid = $this->uri->segment(2);
+//        $orders_noid = $this->uri->segment(2);
+//
+//        $order_bukti = $this->order_bukti->where('orders_noid', $orders_noid)->get();
+//
+//        if ($order_bukti) {
+//            //upload an image options
+//            $config = array();
+//            $config['upload_path'] = './upload';
+//            $config['allowed_types'] = 'gif|jpg|png';
+//            $config['max_size'] = '0';
+//            $config['overwrite'] = TRUE;
+//            $config['encrypt_name'] = TRUE;
+//
+//            $this->load->library('upload', $config);
+//            $this->upload->do_upload('bukti_pembayaran');
+//            $hasil = $this->upload->data();
+//
+//            $konfirmasi = $this->order_bukti->where('orders_noid', $orders_noid)->update(array(
+//                'orders_bukti_nama_rek' => $this->input->post('rek_atasnama'),
+//                'orders_bukti_no_rek' => $this->input->post('nomor_rekening'),
+//                'orders_bukti_bank_nama' => $this->input->post('bank'),
+//                'orders_bukti_nominal' => $this->input->post('total_pembayaran'),
+//                'orders_bukti_foto' => $hasil['file_name'],
+//            ));
+//
+//            if ($konfirmasi) {
+//                $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
+//            }
+//        } else {
+//            //upload an image options
+//            $config = array();
+//            $config['upload_path'] = './upload';
+//            $config['allowed_types'] = 'gif|jpg|png';
+//            $config['max_size'] = '0';
+//            $config['overwrite'] = TRUE;
+//            $config['encrypt_name'] = TRUE;
+//
+//            $this->load->library('upload', $config);
+//            $this->upload->do_upload('bukti_pembayaran');
+//            $hasil = $this->upload->data();
+//
+//            $konfirmasi = $this->order_bukti->insert(array(
+//                'orders_bukti_nama_rek' => $this->input->post('rek_atasnama'),
+//                'orders_bukti_no_rek' => $this->input->post('nomor_rekening'),
+//                'orders_bukti_bank_nama' => $this->input->post('bank'),
+//                'orders_bukti_nominal' => $this->input->post('total_pembayaran'),
+//                'orders_bukti_foto' => $hasil['file_name'],
+//                'orders_noid' => $orders_noid
+//            ));
+//
+//            if ($konfirmasi) {
+//                $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
+//            }
+//        }
+//
+//        redirect('checkout/' . $this->uri->segment(2) . '/sukses');
 
+        $orders_noid = $this->uri->segment(2);
         $order_bukti = $this->order_bukti->where('orders_noid', $orders_noid)->get();
 
+        //upload an image options
+        $config = array();
+        $config['upload_path'] = './upload';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = '0';
+        $config['overwrite'] = TRUE;
+        $config['encrypt_name'] = TRUE;
+
+        $this->load->library('upload', $config);
+        $this->upload->do_upload('bukti_pembayaran');
+        $hasil = $this->upload->data();
+
+
+        $konfirmasi_array = array(
+            'orders_bukti_nama_rek' => $this->input->post('rek_atasnama'),
+            'orders_bukti_no_rek' => $this->input->post('nomor_rekening'),
+            'orders_bukti_bank_nama' => $this->input->post('bank'),
+            'orders_bukti_nominal' => $this->input->post('total_pembayaran'),
+            'orders_bukti_foto' => $hasil['file_name'],
+            'orders_noid' => $orders_noid
+        );
+
         if ($order_bukti) {
-            //upload an image options
-            $config = array();
-            $config['upload_path'] = './upload';
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size'] = '0';
-            $config['overwrite'] = TRUE;
-            $config['encrypt_name'] = TRUE;
-
-            $this->load->library('upload', $config);
-            $this->upload->do_upload('bukti_pembayaran');
-            $hasil = $this->upload->data();
-
-            $konfirmasi = $this->order_bukti->where('orders_noid', $orders_noid)->update(array(
-                'orders_bukti_nama_rek' => $this->input->post('rek_atasnama'),
-                'orders_bukti_no_rek' => $this->input->post('nomor_rekening'),
-                'orders_bukti_bank_nama' => $this->input->post('bank'),
-                'orders_bukti_nominal' => $this->input->post('total_pembayaran'),
-                'orders_bukti_foto' => $hasil['file_name'],
-            ));
-
-            if ($konfirmasi) {
-                $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
-            }
+            // update
+            $this->order_bukti->update($konfirmasi_array, 'orders_noid');
+            $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
+            redirect('checkout/' . $this->uri->segment(2) . '/sukses');
         } else {
-            //upload an image options
-            $config = array();
-            $config['upload_path'] = './upload';
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size'] = '0';
-            $config['overwrite'] = TRUE;
-            $config['encrypt_name'] = TRUE;
+            // insert
+            $this->order_bukti->insert($konfirmasi_array);
 
-            $this->load->library('upload', $config);
-            $this->upload->do_upload('bukti_pembayaran');
-            $hasil = $this->upload->data();
-
-            $konfirmasi = $this->order_bukti->insert(array(
-                'orders_bukti_nama_rek' => $this->input->post('rek_atasnama'),
-                'orders_bukti_no_rek' => $this->input->post('nomor_rekening'),
-                'orders_bukti_bank_nama' => $this->input->post('bank'),
-                'orders_bukti_nominal' => $this->input->post('total_pembayaran'),
-                'orders_bukti_foto' => $hasil['file_name'],
-                'orders_noid' => $orders_noid
-            ));
-
-            if ($konfirmasi) {
-                $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
-            }
+            $this->order->where('orders_noid', $orders_noid)->update(array('orders_status' => 3));
+            redirect('checkout/' . $this->uri->segment(2) . '/sukses');
         }
-
-        redirect('checkout/' . $this->uri->segment(2) . '/sukses');
     }
 
     public function sukses()
