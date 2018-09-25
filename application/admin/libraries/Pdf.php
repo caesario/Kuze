9a23e91be2ec2874f1dd50607e4aec5a7ddb706a
Muhammad Irfan <irfan@bukaide.com>
2018-09-25 23:01:00 +0700

    Update

--- application/admin/libraries/Pdf.php
+++ application/admin/libraries/Pdf.php
@@ -2,17 +2,6 @@
 
 class Pdf
 {
-    function __construct()
-    {
-        include_once APPPATH . '/third_party/mpdf/vendor/autoload.php';
-    }
-
-    function pdf()
-    {
-        $CI = &get_instance();
-        log_message('Debug', 'mPDF class is loaded.');
-    }
-
     function load($param = [])
     {
         return new \Mpdf\Mpdf($param);
