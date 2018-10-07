f76175b26b8853d88f35ce079675e75b304421e2
caesario <32963391+caesario@users.noreply.github.com>
2018-10-08 00:11:04 +0700

    Update Slider, tinggal di integrasiin aja fan

--- application/user/views/Home.php
+++ application/user/views/Home.php
@@ -3,6 +3,25 @@ include "layout/Header.php";
 include "layout/Menu.php";
 ?>
 
+    <div id="carouselExampleSlidesOnly" class="carousel slide f-z-slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner c-slideshow">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/banner-test3.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/banner-test3.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/banner-test3.jpg" alt="Third slide">
+            </div>
+        </div>
+    </div>
+
     <div class="spinner">
         <div class="rect1"></div>
         <div class="rect2"></div>
