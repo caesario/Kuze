f9da5975370db5c1e92d2b6b23e3541f67c90ea6
Muhammad Irfan <irfan@bukaide.com>
2018-10-08 02:15:08 +0700

    Update

--- application/user/controllers/Item.php
+++ application/user/controllers/Item.php
@@ -72,18 +72,24 @@ class Item extends CI_Controller
     public function sale_item()
     {
         $hasil = array();
-        $data = $this->item->as_array()->where_i_sale('1')
-            ->order_by('created_at', 'DESC')
-            ->limit(8)
-            ->get_all();
+        if (!$this->cache->get('sale_item')) {
+            $data = $this->item->as_array()->where_i_sale('1')
+                ->order_by('created_at', 'DESC')
+                ->limit(8)
+                ->get_all();
 
-        foreach ($data as $k => $v) {
+            foreach ($data as $k => $v) {
 
-            $hasil[$k]['i_kode'] = $v['i_kode'];
-            $hasil[$k]['i_url'] = $v['i_url'];
-            $hasil[$k]['i_nama'] = $v['i_nama'];
-            $hasil[$k]['i_hrg'] = $v['i_hrg'];
-            $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+                $hasil[$k]['i_kode'] = $v['i_kode'];
+                $hasil[$k]['i_url'] = $v['i_url'];
+                $hasil[$k]['i_nama'] = $v['i_nama'];
+                $hasil[$k]['i_hrg'] = $v['i_hrg'];
+                $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            }
+
+            $this->cache->save('sale_item', $hasil, 300);
+        } else {
+            $hasil = $this->cache->get('sale_item');
         }
 
         echo json_encode($hasil);
@@ -92,35 +98,52 @@ class Item extends CI_Controller
     public function kbest_seller()
     {
         $hasil = array();
-        $data = $this->item->as_array()->where_i_best('1')
-            ->order_by('created_at', 'DESC')
-            ->get_all();
+        if (!$this->cache->get('best_seller')) {
+//            echo 'Not Cached';
+            $data = $this->item->as_array()->where_i_best('1')
+                ->order_by('created_at', 'DESC')
+                ->get_all();
 
-        foreach ($data as $k => $v) {
-            $hasil[$k]['i_kode'] = $v['i_kode'];
-            $hasil[$k]['i_url'] = $v['i_url'];
-            $hasil[$k]['i_nama'] = $v['i_nama'];
-            $hasil[$k]['i_hrg'] = $v['i_hrg'];
-            $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            foreach ($data as $k => $v) {
+                $hasil[$k]['i_kode'] = $v['i_kode'];
+                $hasil[$k]['i_url'] = $v['i_url'];
+                $hasil[$k]['i_nama'] = $v['i_nama'];
+                $hasil[$k]['i_hrg'] = $v['i_hrg'];
+                $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            }
+            $this->cache->save('best_seller', $hasil, 300);
+
+        } else {
+//            echo 'cached';
+            $hasil = $this->cache->get('best_seller');
         }
+
         echo json_encode($hasil);
     }
 
     public function knew_arrival()
     {
         $hasil = array();
-        $data = $this->item->as_array()->where_i_new('1')
-            ->order_by('created_at', 'DESC')
-            ->get_all();
+        if (!$this->cache->get('new_arrival')) {
 
-        foreach ($data as $k => $v) {
+            $data = $this->item->as_array()->where_i_new('1')
+                ->order_by('created_at', 'DESC')
+                ->get_all();
 
-            $hasil[$k]['i_kode'] = $v['i_kode'];
-            $hasil[$k]['i_url'] = $v['i_url'];
-            $hasil[$k]['i_nama'] = $v['i_nama'];
-            $hasil[$k]['i_hrg'] = $v['i_hrg'];
-            $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            foreach ($data as $k => $v) {
+
+                $hasil[$k]['i_kode'] = $v['i_kode'];
+                $hasil[$k]['i_url'] = $v['i_url'];
+                $hasil[$k]['i_nama'] = $v['i_nama'];
+                $hasil[$k]['i_hrg'] = $v['i_hrg'];
+                $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            }
+
+            $this->cache->save('new_arrival', $hasil, 300);
+        } else {
+            $hasil = $this->cache->get('new_arrival');
         }
+
 
         echo json_encode($hasil);
     }
@@ -128,17 +151,23 @@ class Item extends CI_Controller
     public function ksale_item()
     {
         $hasil = array();
-        $data = $this->item->as_array()->where_i_sale('1')
-            ->order_by('created_at', 'DESC')
-            ->get_all();
+        if (!$this->cache->get('sale_item')) {
+            $data = $this->item->as_array()->where_i_sale('1')
+                ->order_by('created_at', 'DESC')
+                ->get_all();
 
-        foreach ($data as $k => $v) {
+            foreach ($data as $k => $v) {
 
-            $hasil[$k]['i_kode'] = $v['i_kode'];
-            $hasil[$k]['i_url'] = $v['i_url'];
-            $hasil[$k]['i_nama'] = $v['i_nama'];
-            $hasil[$k]['i_hrg'] = $v['i_hrg'];
-            $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+                $hasil[$k]['i_kode'] = $v['i_kode'];
+                $hasil[$k]['i_url'] = $v['i_url'];
+                $hasil[$k]['i_nama'] = $v['i_nama'];
+                $hasil[$k]['i_hrg'] = $v['i_hrg'];
+                $hasil[$k]['i_img'] = $this->get_image($v['i_kode']);
+            }
+
+            $this->cache->save('sale_item', $hasil, 300);
+        } else {
+            $hasil = $this->cache->get('sale_item');
         }
 
         echo json_encode($hasil);
